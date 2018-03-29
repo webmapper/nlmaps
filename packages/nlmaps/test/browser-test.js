aadbcc5e4a1cf98b41178a458fbb265d93042ab5
Hans Fast <hans@surfacemarkup.net>
2018-03-29 11:57:08 +0200

    add browser test for nlmaps package

--- packages/nlmaps/test/browser-test.js
+++ packages/nlmaps/test/browser-test.js
@@ -1,13 +1,14 @@
-//import {bgLayer, geoLocatorControl} from '../../nlmaps-leaflet/build/nlmaps-leaflet.es.js';
+import {bgLayer, geoLocatorControl} from '../../nlmaps-leaflet/build/nlmaps-leaflet.es.js';
+import geolocator from '../../nlmaps-geolocator/build/nlmaps-geolocator.es.js';
 
-//let map = L.map('mapdiv').setView([52, 5], 10);
-//let layers = {
-  //standaard: bgLayer(), 
-//};
-//layers.standaard.addTo(map);
-//layers.pastel = bgLayer('pastel');
-//layers.grijs = bgLayer('grijs');
-//L.control.layers(layers).addTo(map);
-//let geo = geolocator();
-//geoLocatorControl(geo).addTo(map);
+let map = L.map('mapdiv').setView([52, 5], 10);
+let layers = {
+  standaard: bgLayer(), 
+};
+layers.standaard.addTo(map);
+layers.pastel = bgLayer('pastel');
+layers.grijs = bgLayer('grijs');
+L.control.layers(layers).addTo(map);
+let geo = geolocator();
+geoLocatorControl(geo).addTo(map);
 
