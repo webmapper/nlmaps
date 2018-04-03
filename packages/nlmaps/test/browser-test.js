687d311813a31e840e1753d994b849f7f1a18949
Hans Fast <hans@surfacemarkup.net>
2018-04-03 10:13:07 +0200

    stub of eventemitter for clicks

--- packages/nlmaps/test/browser-test.js
+++ packages/nlmaps/test/browser-test.js
@@ -1,14 +1,5 @@
-import {bgLayer, geoLocatorControl} from '../../nlmaps-leaflet/build/nlmaps-leaflet.es.js';
-import geolocator from '../../nlmaps-geolocator/build/nlmaps-geolocator.es.js';
 
-let map = L.map('mapdiv').setView([52, 5], 10);
-let layers = {
-  standaard: bgLayer(), 
-};
-layers.standaard.addTo(map);
-layers.pastel = bgLayer('pastel');
-layers.grijs = bgLayer('grijs');
-L.control.layers(layers).addTo(map);
-let geo = geolocator();
-geoLocatorControl(geo).addTo(map);
-
+let map = nlmaps.createMap({target: 'mapdiv', center: {latitude: 5, longitude: 52}})
+nlmaps.clickprovider(map).subscribe(function(e){
+  console.log(e)
+});
