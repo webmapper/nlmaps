c1b3660eda6b9026b4e389c1367463a4349b9855
Hans Fast <hans@surfacemarkup.net>
2018-04-12 10:14:43 +0200

    clicking

--- packages/nlmaps/src/index.js
+++ packages/nlmaps/src/index.js
@@ -1,4 +1,4 @@
-//import emitonoff from 'emitonoff';
+import emitonoff from 'emitonoff';
 import { bgLayer as bgL,
          overlayLayer as overlayL,
          markerLayer as markerL,
@@ -324,7 +324,8 @@ nlmaps.geoLocate = function(map, useropts = {}){
 
 nlmaps.clickprovider = function(map) {
   return {
-    subscribe: function (callback) {
+    subscribe: return {
+      function (callback) {
       map.on('click', (e) => {
         callback(e.latlng)
       })
